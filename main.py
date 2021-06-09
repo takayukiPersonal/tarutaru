0990e2e4abe2c2e08095b74e77e496e80e63071a
関　貴志 <ts@sekitakashinoMacBook-Pro.local>
2021-06-09 09:19:29 +0900

    最大試行回数追加

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class TaruTaru:
     def __init__(self, finish_word: str, num_finish: int):
         self.finish_word = finish_word
         self.num_finish = num_finish
+        self.max_try_count = 100000
         self.key_dict = {}
         self.word_dict = {}
 
@@ -28,27 +29,25 @@ class TaruTaru:
     def calc_tarutaru(self, begin_word: str):
         print(begin_word)
         begin_word_last_letter = begin_word[-1]
-
         self.open_csv()
 
-        # print(self.word_dict)
-        # input('')
-
         for num in range(1, self.num_finish+1):
-            while True:
+            try_count = 0
+            while try_count < self.max_try_count:
+                try_count += 1
                 name_kana = self.key_dict[random.choice(range(len(self.word_dict)))]
                 name = self.word_dict[name_kana]
                 first_letter = name_kana[0]
                 last_letter = name_kana[-1]
                 if num != self.num_finish:
-                    if (first_letter == begin_word_last_letter and last_letter != 'ン' and last_letter != 'ー'):
-                        print(f'{name}({name_kana})')
+                    if first_letter == begin_word_last_letter and last_letter != 'ン' and last_letter != 'ー' and last_letter != self.finish_word[0]:
+                        print(f'{name_kana}({name})')
                         begin_word_last_letter = last_letter
                         # del self.word_dict[name_kana]
                         break
                 else:
-                    if (first_letter == begin_word_last_letter and last_letter == self.finish_word[0]):
-                        print(f'{name}({name_kana})')
+                    if first_letter == begin_word_last_letter and last_letter == self.finish_word[0]:
+                        print(f'{name_kana}({name})')
                         print(self.finish_word)
                         return
 
